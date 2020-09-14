e1d3436038232b12acb2bc0700cc6fd1f8e2571c
jchanme <39323414+jchanme@users.noreply.github.com>
2020-09-14 17:21:39 +0800

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Slides URL',
+    'version': '1.0',
+    'summary': 'Change the URL of the app Slides',
+    'website': 'https://www.safyric.com',
+    'category': 'Website',
+    'description': """
+""",
+    'depends': ['website_slides'],
+    'data': [
+    ],
+    'installable': True,
+}
