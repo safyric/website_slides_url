49d83c1b1c6bd87fd4832f489e0565073e64d2ab
jchanme <39323414+jchanme@users.noreply.github.com>
2020-09-14 17:34:52 +0800

    Update main.py

--- controllers/main.py
+++ controllers/main.py
@@ -1,2 +1,9 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import http
+from odoo.http import request
+from odoo.addons.website_slides.controllers.main import Slides
+
+class WebsiteSlidesInherit(Slides):
+  @http.route()
