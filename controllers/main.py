e67d7173fa41f645a2324d62c5c01126d30716eb
jchanme <39323414+jchanme@users.noreply.github.com>
2020-09-15 01:23:01 +0800

    Update main.py

--- controllers/main.py
+++ controllers/main.py
@@ -3,7 +3,9 @@
 
 from odoo import http
 from odoo.http import request
-from odoo.addons.website_slides.controllers.main import Slides
+from odoo.addons.website_slides.controllers.main import WebsiteSlides
 
-class WebsiteSlidesInherit(Slides):
-  @http.route()
+class WebsiteSlidesInherit(WebsiteSlides):
+    @http.route('/resources')
+    def slides_index(self):
+        return super(WebsiteSlidesInherit,self).slides_index(self)
