1f3e1321536d36ca1531fb2e9df6f3ed2d3208ac
jchanme <39323414+jchanme@users.noreply.github.com>
2020-09-19 21:24:21 +0800

    Update main.py

--- controllers/main.py
+++ controllers/main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import http
+from odoo import http, _
 from odoo.http import request
 from odoo.addons.website_slides.controllers.main import WebsiteSlides
 
